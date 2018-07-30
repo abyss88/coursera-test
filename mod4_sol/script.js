426858b006d47c0a042c314ee3960f6b0db4c8aa
김재한님/Biz.지원 Cell <1104702@sktelecom.com>
2018-07-30 17:25:50 +0900

    small changes

---
+++ mod4_sol/script.js
@@ -0,0 +1,23 @@
+
+
+(function () {
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  // Loop over the names array 
+  for (var i = 0; i < names.length; i++) {
+
+  // Retrieve the first letter of the current name in the loop.
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  // Compare the 'firstLetter' to lower case 'j'
+  // If the same, call byeSpeaker's 'speak' method with the current name
+  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
+  // name in the loop.
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+
+})();
